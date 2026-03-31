7c4af460076ec0f4e8fe655c689c3379f178d84d
Yuri <ndcyuri@gmail.com>
2026-03-31 16:09:29 -0300

    feat: created role repository

---
+++ src/main/java/com/management/inventory/auth/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.management.inventory.auth.repository;
+
+import com.management.inventory.auth.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}

142fcb5e5ff9c7263ba33d2dd5117f5dc26e48a4
Yuri <ndcyuri@gmail.com>
2026-03-31 01:43:42 -0300

    feat: created role entity

---
+++ src/main/java/com/management/inventory/auth/entity/Role.java
@@ -0,0 +1,24 @@
+package com.management.inventory.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "role")
+@Table(name = "role")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String role;
+
+}

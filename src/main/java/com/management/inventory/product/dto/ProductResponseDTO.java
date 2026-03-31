c3ad0540bc22c136d9e85c58d4b0cf9c37b7fa4c
Yuri <ndcyuri@gmail.com>
2026-03-31 16:27:02 -0300

    feat: created product response dto

---
+++ src/main/java/com/management/inventory/product/dto/ProductResponseDTO.java
@@ -0,0 +1,19 @@
+package com.management.inventory.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductResponseDTO {
+
+    private Long id;
+    private String name;
+    private String code;
+    private int quantity;
+
+}

71c2bce5e2666e17aedbc755a63c8f7e290bbc90
Yuri <ndcyuri@gmail.com>
2026-03-31 02:26:44 -0300

    feat: created Poduct repository

---
+++ src/main/java/com/management/inventory/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.management.inventory.product.repository;
+
+import com.management.inventory.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}

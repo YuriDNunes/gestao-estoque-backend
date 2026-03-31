53663d83b1b6755d9f238c012b6fed27318febba
Yuri <ndcyuri@gmail.com>
2026-03-31 02:06:46 -0300

    feat: created User entity with relationship to Role

---
+++ src/main/java/com/management/inventory/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.management.inventory.user.entity;
+
+import com.management.inventory.auth.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "user")
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+
+    @Column(length = 45, nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 45, unique = true, nullable = false)
+    private String register;
+
+    @Column(nullable = false)
+    private Boolean access;
+
+    @ManyToOne
+    @JoinColumn(name = "role_idrole", nullable = false)
+    private Role role;
+
+}

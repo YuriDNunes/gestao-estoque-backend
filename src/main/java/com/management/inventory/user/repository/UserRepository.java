6782cd5849ca61076294c2f2d392d2ac7d2db310
Yuri <ndcyuri@gmail.com>
2026-03-31 02:26:57 -0300

    feat: created User repository

---
+++ src/main/java/com/management/inventory/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.management.inventory.user.repository;
+
+import com.management.inventory.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

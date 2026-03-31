862e07c36e78d8f1c61c7330026538391197905f
Yuri <ndcyuri@gmail.com>
2026-03-31 16:24:02 -0300

    feat: created user response dto

---
+++ src/main/java/com/management/inventory/user/dto/UserResponseDTO.java
@@ -0,0 +1,21 @@
+package com.management.inventory.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String register;
+    private Boolean access;
+    private String role;
+
+}

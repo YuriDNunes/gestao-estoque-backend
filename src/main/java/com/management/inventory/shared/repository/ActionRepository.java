3507476154dc48d1939b915b0c1a312a9bb122ab
Yuri <ndcyuri@gmail.com>
2026-03-31 16:10:29 -0300

    feat: created action repository

---
+++ src/main/java/com/management/inventory/shared/repository/ActionRepository.java
@@ -0,0 +1,9 @@
+package com.management.inventory.shared.repository;
+
+import com.management.inventory.shared.entity.Action;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActionRepository extends JpaRepository<Action, Long> {
+}

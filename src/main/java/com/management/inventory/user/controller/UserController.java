c9a1daf3d4ab425f3bc3b6b1ad0e6825f5042b89
Yuri <ndcyuri@gmail.com>
2026-04-01 14:48:41 -0300

    feat: list all users endpoint created

--- src/main/java/com/management/inventory/user/controller/UserController.java
+++ src/main/java/com/management/inventory/user/controller/UserController.java
@@ -4,10 +4,9 @@ import com.management.inventory.user.dto.UserRequestDTO;
 import com.management.inventory.user.dto.UserResponseDTO;
 import com.management.inventory.user.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,6 +18,11 @@ public class UserController {
     @PostMapping
     public UserResponseDTO createUser(@RequestBody UserRequestDTO user){
         return services.create(user);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> listUsers(){
+        return services.listUsers();
     }
 
 }

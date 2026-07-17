081a79c27a3e20bcb2756ec0494bc5a4250a136e
Yuri <ndcyuri@gmail.com>
2026-07-17 13:39:21 -0300

    feature: put PasswordEncoder out of the SecurityConfig

--- src/main/java/com/management/inventory/config/SecurityConfig.java
+++ src/main/java/com/management/inventory/config/SecurityConfig.java
@@ -5,13 +5,12 @@ import com.management.inventory.security.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,11 +22,6 @@ public class SecurityConfig {
 
     public SecurityConfig(JwtTokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -55,7 +49,7 @@ public class SecurityConfig {
                             .requestMatchers("/api/**").authenticated()
                             .requestMatchers("/users").denyAll()
                 )
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .build();
     }
 }
